a081ec16a43b178a5949d72e74a8e5c56f443449
Rahul Pandey <rahulprogrammerp@gmail.com>
2024-10-08 09:23:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+require('dotenv').config()
+const express =  require("express")
+const app = express()
+const port =4000;
+
+app.get("/" , (req,res)=>{
+    res.send("Hey server 1");
+})
+
+
+app.get("/login", (req,res)=>{
+    res.send("<h1> Please login at chai and code </h1>")
+})
+
+app.get("/youtube", (req,res)=>{
+    res.send("<h2> Chai aur code </h2>")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is istening ${port}`)
+})
